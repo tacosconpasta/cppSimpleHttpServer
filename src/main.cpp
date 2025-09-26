51c2dc9fc848a624291500df6a9145c7096c21e1
Raúl Villarreal <raulevillar30@gmail.com>
2025-09-26 01:56:12 -0500

    portNumber Argument (Addition): Now the main function will be able to receive the port argument, for which the user wants to run the server on

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,55 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 
 int error(const char* msg){
   perror(msg);
   exit(1);
 }
 
-int main (void){
+int main(int argc, char *argv[]){
+  int portNumber;
+
+  //If user doesnt input port number
+  if(argc < 2){
+    std::cerr << "Argument count is less than 2, PORT_NUMBER was NOT provided." << std::endl;
+    return 1;
+  }
+
+  //If user inputs too many arguments
+  if(argc > 2){
+    std::cerr << "Too many arguments were provided." << std::endl;
+    return 1;
+  }
+
+  //Parse portNumber parameter for checking
+  portNumber = atoi(argv[1]);
+  int digits = (portNumber == 0) ? 1 : (int)std::log10(std::abs(portNumber)) + 1;
+
+  //If PORT is not safe for "toying around" return; CHANGE THIS LINE AT OWN EXPENSE
+  if (digits != 4 || portNumber < 1024 || portNumber > 9999) {
+      std::cerr << "Port Number is not safe for testing" << std::endl;
+      return 1;
+  }
+
+
   Socket socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   Server server;
+
+  //Structure that holds server address and port
+  struct sockaddr_in serverAddress;
+
+  //Set up the serverAddress
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_addr.s_addr = INADDR_LOOPBACK;
+
+  //Parse portNumber and add it to the serverAddress struct
+  portNumber = atoi(argv[1]);
+  serverAddress.sin_port = htons(portNumber);
   
+  std::cout << portNumber << std::endl;
+
   server.setConnectionSocket(socket);
 
   Socket receivingSocket = server.getConnectionSocket();
